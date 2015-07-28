17d8245836c068f7973127201e3e2652515524f0
NicolsonM <fedoraman@ukr.net>
2015-07-28 07:23:17 +0300

    7:23

--- Site js/js/slider.js
+++ Site js/js/slider.js
@@ -1,15 +1,17 @@
 // JavaScript source code
-var imgSldr = new Object();
-var slider = document.getElementById("slider");
-imgSldr.leftS = document.getElementById("buts1");
-imgSldr.rightS = document.getElementById("buts2");
-leftS.addEventListener("click",
-    function () {
-        slider.style.left = "960px";
-    }, false
-    );
-rightS.addEventListener("click",
-    function () {
-        slider.style.right = "960px";
-    }, false
-    );
+window.onload = function () {
+    var slider = document.getElementById("slider");
+   prev = document.getElementById("buts1");
+    next = document.getElementById("buts2");
+    prev.addEventListener("click",
+        function () {
+            slider.style.left = "960px";
+        }, false
+        );
+    next.addEventListener("click",
+        function () {
+            slider.style.right = "960px";
+        }, false
+        );
+
+}
