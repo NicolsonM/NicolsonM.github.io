3407750836be959b26fb1f0b7f2d87346ac1b4b8
NicolsonM <fedoraman@ukr.net>
2015-07-28 20:50:59 +0300

    20:50

---
+++ Site js/js/menu.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+function showElement(layer) {
+    var myLayer = document.getElementById(layer);
+    if (myLayer.style.display == "none") {
+        myLayer.style.display = "block";
+        myLayer.backgroundPosition = "top";
+    } else {
+        myLayer.style.display = "none";
+    }
+}
